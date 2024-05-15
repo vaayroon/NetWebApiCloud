25509f197d40d40f93a1380074d987452d4e9d98
Bryan Kevin Condor Romero <vaayroon@outlook.com>
2024-05-15 22:46:24 +0200

    chore: Add Domain project files and classes

---
+++ src/Domain/ShareKernel/ErrorType.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.ShareKernel;
+
+public enum ErrorType
+{
+    /// <summary>
+    /// Represents a failure error type.
+    /// </summary>
+    Failure = 0,
+
+    /// <summary>
+    /// Represents a validation error type.
+    /// </summary>
+    Validation = 1,
+
+    /// <summary>
+    /// Represents a not found error type.
+    /// </summary>
+    NotFound = 2,
+
+    /// <summary>
+    /// Represents a conflict error type.
+    /// </summary>
+    Conflict = 3
+}
---
+++ src/Domain/ShareKernel/ResultT.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Domain.ShareKernel;
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    protected internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    [NotNull]
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can't be accessed.");
+
+    public static implicit operator Result<TValue>(TValue? value) =>
+        value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+}
